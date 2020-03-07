2b86587b6c068418213f95576a5eb903a42e5781
sardul3 <poudelsagar530@gmail.com>
2020-03-07 00:47:34 -0600

    added location and nearest station

--- src/app/stations/stations.page.ts
+++ src/app/stations/stations.page.ts
@@ -16,6 +16,8 @@ export class StationsPage implements OnInit {
   searchInput: string;
   mode: string;
   myStations = [];
+  myLon: number;
+  myLat: number;
 
   constructor(private stationsService: StationsService,
               private toastController: ToastController) { }
@@ -24,16 +26,33 @@ export class StationsPage implements OnInit {
       this.stationsService.fetchStations().subscribe(() => {
         this.isLoading = false;
         this.stations = this.stationsService.getStations();
+
+        Geolocation.getCurrentPosition().then(loc => {
+          console.log(loc.coords.latitude, loc.coords.longitude);
+          this.myLat = loc.coords.latitude;
+          this.myLon = loc.coords.longitude;
+          console.log(typeof loc.coords.longitude);
+
+          this.myStations.push(this.stationsService.getNearByStations(this.myLat, this.myLon));
+
+
+        });
+
       });
 
-      const coordinates = Geolocation.getCurrentPosition();
-      console.log('Current', coordinates);
+
     }
 
 ionViewWillEnter() {
- this.stations = this.stationsService.getStations();
  this.searchInput = '';
  this.myStations = this.stationsService.getMyStations();
+
+ if (this.mode === 'my') {
+  this.stations = this.stationsService.getMyStations();
+} else {
+  this.stations = this.stationsService.getStations();
+}
+
 }
 
 searchStations() {
--- src/app/stations/stations.service.ts
+++ src/app/stations/stations.service.ts
@@ -11,6 +11,7 @@ export class StationsService {
   stations = [];
   stationData = [];
   myStations = [];
+  stationDataJSON = {keys: []};
 
 
   constructor(private http: HttpClient) { }
@@ -20,6 +21,18 @@ export class StationsService {
       // tslint:disable-next-line: forin
       for (const line in res.split('\n')) {
           this.stations.push(res.split('\n')[line]);
+
+          const keysValue = res.split('\n')[0];
+          const keysArr = keysValue.split(',');
+          this.stationDataJSON.keys = keysArr;
+
+          const resWithoutHeader = res.split('\n');
+          const values = (resWithoutHeader[line]);
+          const valArr = values.split(',');
+          const valueKey = valArr[0];
+          const valValue = valArr;
+          this.stationDataJSON[valueKey] = [ valValue ];
+
       }
     }));
   }
@@ -39,12 +52,10 @@ export class StationsService {
     const date = new Date();
     // tslint:disable-next-line: max-line-length
     const formattedDate = date.getFullYear() + '' + this.appendLeadingZeroes((date.getMonth() + 1)) + '' + this.appendLeadingZeroes(date.getDate()) + '000000';
-    console.log(formattedDate);
 
     // tslint:disable-next-line: max-line-length
     return this.http.get(`http://secondary.mesonet.ksu.edu/rest/stationdata/?stn=${stationName}&int=day&t_start=${formattedDate}&t_end=${formattedDate}`,
                   {responseType: 'text'}).pipe(take(1), tap(res => {
-                    console.log(res);
                     // tslint:disable-next-line: forin
                     for (const line in res.split('\n')) {
                       this.stationData.push(res.split('\n')[line]);
@@ -85,5 +96,40 @@ export class StationsService {
         return (st.split(',')[0].toLowerCase()).startsWith(searchTerm.toLowerCase()) && index > 0;
     });
   }
+}
+
+getNearByStations(lat: number, lon: number) {
+  const keys = this.stationDataJSON.keys;
+  const latKeyJSON = keys.indexOf('LATITUDE');
+  const lonKeyJSON = keys.indexOf('LONGITUDE');
+  let stationLat: number;
+  let stationLon: number;
+  const distances = [];
+  let closest: number;
+  let closestIndex: number;
+
+  this.stations.forEach(st => {
+    const stationName = st.split(',')[0];
+    const stationRow = this.stationDataJSON[stationName];
+    stationLat = Number(stationRow[0][latKeyJSON]);
+    stationLon = Number(stationRow[0][lonKeyJSON]);
+    console.log(typeof stationRow[0][lonKeyJSON])
+    console.log(stationRow, typeof stationLat, typeof stationLon);
+
+    const dx = lat - stationLat;
+    const dy = lon - stationLon;
+    console.log(dx, dy);
+
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    distances.push(distance);
+    closest = Math.min(...distances.slice(1));
+
+    closestIndex = distances.indexOf(closest);
+
+  });
+ // return (this.stations[closestIndex].split(','))[keys.indexOf('NAME')];
+  console.log(this.stations[closestIndex]);
+  return this.stations[closestIndex];
+
 }
 }
